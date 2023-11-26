1c9f0dc3cd32a2db4d89ac4072ee1e1e7e48e933
MohamedAshrafElRokh <mohamed.ashraf.elrokh@gmail.com>
2023-11-26 23:25:19 +0200

    created type file for the data (userDetails, Product, price, subscription) and types_db.ts for the data of the table in supabase

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  return <div className="text-3xl text-white">modal</div>;
+};
+
+export default ModalProvider;
---
+++ types.ts
@@ -0,0 +1,51 @@
+import Stripe from "stripe";
+
+export interface UserDetails {
+  id: string;
+  first_name: string;
+  last_name: string;
+  full_name?: string;
+  avatar_url?: string;
+  billind_address?: Stripe.Address;
+  payment_method?: Stripe.PaymentMethod[Stripe.PaymentMethod.Type];
+}
+
+export interface Product {
+  id: string;
+  active?: boolean;
+  name?: string;
+  description?: string;
+  image?: string;
+  metadata?: Stripe.Metadata;
+}
+
+export interface Price {
+  id: string;
+  product_id?: string;
+  active?: boolean;
+  descrption?: string;
+  unit_amount?: number;
+  currency?: string;
+  type?: Stripe.Price.Type;
+  interval?: Stripe.Price.Recurring.Interval;
+  interval_count?: number;
+  trial_period_days?: number | null;
+  metdata?: Stripe.Metadata;
+  products?: Product;
+}
+
+export interface Subscriptions {
+  id: string;
+  user_id: string;
+  status?: Stripe.Subscription.Status;
+  metadata?: Stripe.Metadata;
+  price_id?: string;
+  quantity?: number;
+  cancel_at_period_end?: boolean;
+  ended_at?: string;
+  cancel_at?: string;
+  canceled_at?: string;
+  trial_start?: string;
+  trial_end?: string;
+  price?: Price;
+}
