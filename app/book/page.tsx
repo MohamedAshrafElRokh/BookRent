e16410dc6323e1013bc7ff4255f44bc3edd65718
MohamedAshrafElRokh <mohamed.ashraf.elrokh@gmail.com>
2023-12-03 09:55:01 +0200

    imported Header and created pageContent displays all books]

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -1,7 +1,36 @@
+import Header from "@/components/Header";
 import React from "react";
-
-const Books = () => {
-  return <div>book</div>;
+import ListItems from "@/components/ListItems";
+import { FaHeart } from "react-icons/fa6";
+import { getBooks } from "@/actions/GetData";
+import PageContent from "./PageContent";
+const Books = async () => {
+  const books = await getBooks();
+  return (
+    <div
+      className="   bg-neutral-900 
+  h-full
+  w-full
+  rounded-lg
+  overflow-hidden
+  overflow-y-auto"
+    >
+      <div className="text-2xl text-white font-semibold ">
+        <div>
+          <Header className="text-black">
+            <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+              <ListItems name={"Liked Books"} href={"liked"}>
+                <FaHeart size={26} />
+              </ListItems>
+            </div>
+          </Header>
+        </div>
+        <div>
+          <PageContent books={books} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Books;
