a5085f3a0d6814206aa06c06196e5fbe5ed58739
MohamedAshrafElRokh <mohamed.ashraf.elrokh@gmail.com>
2023-12-06 02:50:20 +0200

    createed link to book/title

--- app/book/PageContent.tsx
+++ app/book/PageContent.tsx
@@ -1,7 +1,11 @@
 import { Book } from "@/types";
+import qs from "query-string";
 import React, { useEffect, useState } from "react";
 import BookModal from "@/components/BookModal";
 import SideBar from "../../components/SideBarBook";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { log } from "console";
 interface PageContentProps {
   books: Book[];
 }
@@ -14,28 +18,7 @@ interface Category {
 export const revalidate = 0;
 
 const PageContent: React.FC<PageContentProps> = ({ books }) => {
-  const categorys: Category[] = [
-    { id: "1", category: "Fantasy" },
-    { id: "2", category: "Crime" },
-    { id: "3", category: "Comics" },
-    { id: "4", category: "Horror" },
-    { id: "5", category: "Fiction" },
-    { id: "6", category: "Novel" },
-  ];
-
-  // const [selected, setSelected] = useState<Category>();
-  // const getCategory = (cat: any) => {
-  //   console.log(cat, "grom page");
-  //   setSelected(cat);
-  // };
-  // console.log("selected", selected);
-
-  // const handelCategory = async (selected: string) => {
-  //   const result = await getBooksByCategory(selected);
-  //   // setCategory(result);
-  //   console.log(res);
-  // };
-  // handelCategory;
+  console.log(books, "hh");
 
   return (
     <div className="w-full flex flex-col justify-around">
@@ -44,13 +27,19 @@ const PageContent: React.FC<PageContentProps> = ({ books }) => {
       </div>
 
       <div className="flex ml-5 mr-5">
-        <div className="flex flex-wrap gap-x-4 gap-y-4">
-          {books?.map((book) => (
-            <div key={book.id} className="flex-none w-48">
-              <BookModal Book={book} />
-            </div>
-          ))}
-        </div>
+        {books.length === 0 ? (
+          <div className="text-white">No Books Available</div>
+        ) : (
+          <div className="flex flex-wrap gap-x-4 gap-y-4">
+            {books?.map((book) => (
+              <div key={book.id} className="flex-none w-48">
+                <Link href={`book/${book.title}`}>
+                  <BookModal Book={book} />
+                </Link>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
